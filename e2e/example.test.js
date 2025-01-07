50e5e08d338acf0bdc599d9327201fecaa768a70
Joseph Sardo <josephs5526@gmail.com>
2025-01-07 18:56:53 -0500

    tweak example

--- e2e/example.test.js
+++ e2e/example.test.js
@@ -1,5 +1,5 @@
-describe("Onboarding Test", () => {
-  it("should have sign up and sign in buttons", async () => {
+describe("Test", () => {
+  it("should have sign up & sign in buttons", async () => {
     await device.launchApp();
     await expect(element(by.id("signUpButton"))).toBeVisible();
     await expect(element(by.id("signInButton"))).toBeVisible();
@@ -11,8 +11,8 @@ describe("Onboarding Test", () => {
   });
 
   it("should show navigate to home screen after signing in successfully", async () => {
-    await element(by.id("emailField")).typeText("irem@test.com");
-    await element(by.id("passwordField")).typeText("123456");
+    await element(by.id("emailField")).typeText("test.com");
+    await element(by.id("passwordField")).typeText("12345");
     await element(by.text("Continue")).tap();
     await expect(element(by.text("My Courses"))).toBeVisible();
   });
